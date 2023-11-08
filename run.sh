1bedb3b5e5885dd13c3ce0b19075a391bed2e82c
Haoqiang Guo <ghaoqi1@grace.ece.lsu.edu>
2023-11-07 22:52:16 -0600

    less threads to process a row

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 rm flex-tile-nperf.csv
 #rm ge_spmm_roofline.csv
 
-k=128
+k=32
 ./flex ./data/pubmed.csv $k
 ./flex ./data/flickr.csv $k
 ./flex ./data/reddit.csv $k
-#./flex ./data/ppi.csv $k
-#./flex ./data/amazon.csv $k
-#./flex ./data/yelp.csv $k
+./flex ./data/ppi.csv $k
+./flex ./data/amazon.csv $k
+./flex ./data/yelp.csv $k
 #./flex ./data/soc-sign-epinions.csv $k
 #./flex ./data/wiki-Vote.csv $k
 #./flex ./data/a_mat.csv 128
